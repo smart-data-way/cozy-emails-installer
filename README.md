1dd37dd2ce699907b5d9c1fa837a1de77d8d56c5
Nicola Palumbo <nikpalumbo@gmail.com>
2015-11-23 16:12:52 +0100

    Installers ready

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # cozy-emails-installer
 Installer for Cozy Email Client
+
+Cozy Emails Installer lets you build installers for Windows or OSX.
+
+## Requirements
+
+* electron 0.34.2
+* node-gyp
+
+
+## Install
+
+    git clone https://github.com/smart-data-way/cozy-emails-installer
+    cd cozy-emails-installer
+    npm install
+
+## Run
+
+    electron .
+
+## Build an installer
+
+For the time being, to create a installer for OSX you need to run it from a OSX system and the same for Windows.
+
+## To build an installer for OSX
+
+    npm run pack:osx
+
+## To build an installer for  Windows
+
+    npm run pack:win
+
--- main.js
+++ main.js
@@ -1,16 +1,10 @@
-var exports = module.exports = {};
 var app = require('app');
 var BrowserWindow = require('browser-window');
 require('crash-reporter').start();
 var mainWindow = null;
-var loginWindow = null;
-var ipc = require('ipc');
-var nodeCrypto = require('crypto');
-var lCookies = [];
 var process = require('process');
-var opener = require('opener');
 var path = require('path-extra');
-//var spawnly = require('spawnly');
+var fs = require('fs');
 
 app.on('window-all-closed', function () {
     app.quit();
@@ -18,97 +12,28 @@ app.on('window-all-closed', function () {
 
 app.on('ready', function () {
 
-    var subemail;
-    var emails_path = path.normalize(path.join(__dirname, 'node_modules', 'emails', 'build', 'server.js'));
-    console.log(emails_path);
-    if (process.platform == 'win32') {
-
-        subemail = require('child_process').fork(emails_path, [], {silent: true});
+    if(process.env.POUCHDB_NAME === undefined) {
+      var defaultDir = path.join(path.homedir(), '.emails');
+      if(!fs.existsSync(defaultDir)) {
+        fs.mkdirSync(defaultDir);
+      }
+      process.chdir(defaultDir);
     }
 
-    else
-        subemail = require('child_process').spawn('node', [emails_path], {'stdio': 'inherit'});
+    var root = path.join(__dirname, 'node_modules', 'cozy-emails', 'build');
+    var start = require(path.join(root, 'server'));
+    start({root:root, dbName: 'db'}, function() {});
 
-    console.log('WebMail should be ready');
+    global.MODEL_MODULE = 'pouchdb';
 
-
-
-    loginWindow = new BrowserWindow({width: 400, height: 450, 'node-integration': true, javascript: true});
-
-    loginWindow.loadUrl('file://' + __dirname + '/login.html');
-
-    loginWindow.on('closed', function () {
-        loginWindow = null;
-    });
-
-    loginWindow.webContents.on("did-frame-finish-load", function () {
-        loginWindow.webContents.session.cookies.remove({name: "sessionid", url: "https://www.sdw.solutions"}, function(error) {
-            if (error) console.log(error);
-           console.log('cookie removed');
-        });
-        loginWindow.webContents.session.cookies.remove({name: "csrftoken", url: "https://www.sdw.solutions"}, function(error) {
-            if (error) console.log(error);
-           console.log('cookie removed');
-        });
-
-    });
-
-    ipc.on('loggedin-message', function (event, msg) {
-        loginWindow.webContents.session.cookies.get({domain: "www.sdw.solutions"}, function (error, cookies) {
-            console.log(loginWindow.webContents);
-            if (error)
-                console.log("Error get cookies Login Window");
-            else {
-                if(cookies.length==2) {
-                    if (cookies[0].name == "csrftoken" && cookies[1].name == "sessionid") {
-                        lCookies = cookies;
-                        mainWindow = new BrowserWindow({
-                            width: 800,
-                            height: 600,
+    mainWindow = new BrowserWindow({
+                            width: 1024,
+                            height: 800,
                             'node-integration': true,
                             javascript: true
                         });
-                        mainWindow.loadUrl('file://' + __dirname + '/main.html');
-                        mainWindow.webContents.on("did-finish-load", function () {
-                        mainWindow.webContents.send('cookies-message', lCookies);
-                            exports.lCookies = lCookies
-                            var watcher = require('./watcher.js');
-                            process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-                        });
 
-
-                        mainWindow.on('closed', function () {
-                            mainWindow = null;
-                            var console = require('console');
-                            console.log('Killed WebMail');
-                            subemail.kill('SIGINT');
-                        });
-                        loginWindow.destroy();
-                    }
-                }
-            }
-        });
-    });
-
-
-    ipc.on('logout', function() {
-
-    loginWindow.webContents.session.cookies.remove({name: "sessionid", url: "https://www.sdw.solutions"},
-        function(error)
-        {
-            if (error)
-                console.log(error)
-            console.log('cookies removed');
-
-        })
-    });
-
-    ipc.on('show-file', function (event, path) {
-        console.log(path);
-        var editor = opener(path);
-
-    });
-
+    mainWindow.loadUrl('file://' + __dirname + '/main.html');
 
 });
 
